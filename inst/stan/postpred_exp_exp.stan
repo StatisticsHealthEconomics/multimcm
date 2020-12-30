1a6b50fb5c2e7aec34e6118d2f03f13e63e3d9b8
Nathan Green <n8thangreen@yahoo.co.uk>
2020-12-30 14:26:40 +0000

    finish postpred.stan. Id still like to remove the need to loop over posterior samples like in the stan manual but dont know how.

--- inst/stan/postpred_exp_exp.stan
+++ inst/stan/postpred_exp_exp.stan
@@ -9,15 +9,33 @@ functions {
     row_vector rate_mean_rng(real curefrac, real lambda0, real lambda_bg, int n) {
 
     row_vector[n] lambda;
-    real ncf;
-
-    ncf = binomial_rng(n, curefrac);
+    real cf[n];
 
     for (i in 1:n) {
-      if (i <= ncf) {
+      cf[i] = uniform_rng(0, 1);
+
+      if (cf[i] < curefrac) {
         lambda[i] = lambda_bg;
       } else {
         lambda[i] = lambda_bg + lambda0;
+      }
+    }
+    return(lambda);
+  }
+
+    // return sample casemix rates
+    row_vector rate_casemix_rng(real curefrac, row_vector lambda0, row_vector lambda_bg, int n) {
+
+    row_vector[n] lambda;
+    real cf[n];
+
+    for (i in 1:n) {
+      cf[i] = uniform_rng(0, 1);
+
+      if (cf[i] < curefrac) {
+        lambda[i] = lambda_bg[i];
+      } else {
+        lambda[i] = lambda_bg[i] + lambda0[i];
       }
     }
     return(lambda);
@@ -27,11 +45,15 @@ functions {
 data {
   int<lower=1> n;
 
-  //in parameters block instead?
-  int n_samples;
+  //TODO: in parameters block instead?
+  int<lower=1> n_samples;
   real<lower=0, upper=1> curefrac[n_samples];
-  real beta_os[n_samples, 2];
-  real beta_bg[n_samples, 2];
+  matrix[n_samples, n] lambda0;
+  matrix[n_samples, n] lambda_bg;
+
+  // using means
+  // real beta_os[n_samples, 2];
+  // real beta_bg[n_samples, 2];
 }
 
 parameters {
@@ -43,25 +65,28 @@ parameters {
 
 generated quantities {
 
+  // // don't 
   // vector[n] lambda_tilde;
   // real<lower=0> t_tilde[n];
   // real lambda0 = exp(beta_os[1]);
   // real lambda_bg = exp(beta_bg[1]);
-  //
   // lambda_tilde = rate_mean_rng(curefrac, lambda0, lambda_bg, n);
-  //
   // t_tilde = exponential_rng(lambda_tilde);
 
   // explicitly loop over samples
   matrix[n_samples, n] lambda_tilde;
   real<lower=0> t_tilde[n_samples, n];
-  real lambda0;
-  real lambda_bg;
+  // real lambda0;
+  // real lambda_bg;
 
   for (i in 1:n_samples) {
-    lambda0 = exp(beta_os[i, 1]);
-    lambda_bg = exp(beta_bg[i, 1]);
-    lambda_tilde[i, 1:n] = rate_mean_rng(curefrac[i], lambda0, lambda_bg, n);
+
+    // // using means
+    // lambda0 = exp(beta_os[i, 1]);
+    // lambda_bg = exp(beta_bg[i, 1]);
+    // lambda_tilde[i, 1:n] = rate_mean_rng(curefrac[i], lambda0, lambda_bg, n);
+
+    lambda_tilde[i, ] = rate_casemix_rng(curefrac[i], lambda0[i, ], lambda_bg[i, ], n);
     t_tilde[i, ] = exponential_rng(lambda_tilde[i, ]);
   }
 
--- scripts/stand_alone_posterior_predictions.R
+++ scripts/stand_alone_posterior_predictions.R
@@ -14,33 +14,58 @@
 
 # explicitly looping over samples
 
-stan_out <- readRDS("~/R/rstanbmcm/data/cf separate/stan_exp_exp_IPI_.Rds")
-xx <- extract(stan_out)
+# stan_out <- readRDS("~/R/rstanbmcm/data/cf separate/stan_exp_exp_IPI_.Rds")
+stan_out <- readRDS("~/R/rstanbmcm/data/cf separate/stan_exp_exp_NIVOLUMAB_.Rds")
+xx <- rstan::extract(stan_out)
 
+# # using lambda means
+# res <-
+#   rstan::stan(
+#     file = here::here("inst/stan/postpred_exp_exp.stan"),
+#     data = list(n = ncol(xx$lp_os),
+#                 n_samples = nrow(xx$beta_os),
+#                 curefrac = xx$cf_os,
+#                 beta_os= xx$beta_os,
+#                 beta_bg = xx$beta_bg),
+#     chains = 1,
+#     warmup = 0,
+#     iter = 1,
+#     algorithm = "Fixed_param")
+
+
+# using lambda case-mix
 res <-
-  stan(file = here::here("inst/stan/postpred_exp_exp.stan"),
-       data = list(n = ncol(xx$lp_os),
-                   n_samples = nrow(xx$beta_os),
-                   curefrac = xx$cf_os,
-                   beta_os= xx$beta_os,
-                   beta_bg = xx$beta_bg),
-       chains = 1,
-       warmup = 0,
-       iter = 1,
-       algorithm = "Fixed_param")
+  rstan::stan(
+    file = here::here("inst/stan/postpred_exp_exp.stan"),
+    data = list(n = ncol(xx$lp_os),
+                n_samples = nrow(xx$beta_os),
+                curefrac = xx$cf_os,
+                lambda0 = xx$lambda_os,
+                lambda_bg = xx$lambda_os_bg),
+    chains = 1,
+    warmup = 0,
+    iter = 1,
+    algorithm = "Fixed_param")
 
+
+# Kaplan-Meier plots
 
 library(survival)
-yy <- extract(res)
-fit <- survfit(Surv(yy$t_tilde[1, 1, ], rep(1, 311)) ~ 1)
+# tx_name <- "IPILIMUMAB"
+tx_name <- "NIVOLUMAB"
+y_tilde <- yy$t_tilde[1, 1, ]
+
+yy <- rstan::extract(res)
+fit <- survfit(Surv(y_tilde, rep(1, length(y_tilde))) ~ 1)
 plot(fit, col = "lightblue", xlim = c(0, 60), conf.int = FALSE)
 
 for (i in 2:90) {
-  fit <- survfit(Surv(yy$t_tilde[1, i, ], rep(1, 311)) ~ 1)
+  fit <- survfit(Surv(yy$t_tilde[1, i, ], rep(1, length(y_tilde))) ~ 1)
   lines(fit, col = "lightblue", conf.int = FALSE)
 }
 
-real_data <- surv_input_data[surv_input_data$TRTA == "IPILIMUMAB", ]
+load("~/R/rstanbmcm/data/surv_input_data.RData")
+real_data <- surv_input_data[surv_input_data$TRTA == tx_name, ]
 fit <- survfit(Surv(real_data$os, real_data$os_event) ~ 1)
 lines(fit, lwd = 2.5, conf.int = FALSE)
 

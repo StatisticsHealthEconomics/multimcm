a2709f652a5859aa4df9fe750894f9feb336f8c6
Nathan Green <n8thangreen@yahoo.co.uk>
2020-12-31 10:27:16 +0000

    extract out functions plot_post_pred_KM()

---
+++ R/plot_post_pred_KM.R
@@ -0,0 +1,74 @@
+
+#' plot_post_pred_KM
+#' 
+#' @param tx_name "IPILIMUMAB", "NIVOLUMAB", "NIVOLUMAB+IPILIMUMAB"
+#' @param orig_data
+#' @param fileloc_out
+#' 
+#' @return 
+#' 
+#' @import survival
+#' @export
+#'
+#' @examples 
+#' 
+#' tx_name <- "IPILIMUMAB"
+#' orig_data <- load("~/R/rstanbmcm/data/surv_input_data.RData")
+#' fileloc_out <- paste0("plots/post_pred_cfsep_exp_exp_", tx_name, ".png")
+#' plot_post_pred_KM(tx_name, orig_data, fileloc_out)
+#' 
+plot_post_pred_KM <- function(tx_name,
+                              orig_data,
+                              fileloc_out) {
+  
+  n_post <- 90
+  real_data <- orig_data[orig_data$TRTA == tx_name, ]
+  yy <- rstan::extract(res)
+  
+  png(filename = fileloc_out)
+  par(mfrow = c(1,2))
+  
+  # os
+  y_tilde <- yy$t_os_tilde[1, 1, ]
+  fit <- survfit(Surv(y_tilde, rep(1, length(y_tilde))) ~ 1)
+  plot(fit,
+       col = "lightblue",
+       xlim = c(0, 60),
+       conf.int = FALSE,
+       main = "OS",
+       ylab = "Survival",
+       xlab = "Month",
+       bty = "n")
+  for (i in 2:n_post) {
+    fit <- survfit(Surv(yy$t_os_tilde[1, i, ], rep(1, length(y_tilde))) ~ 1)
+    lines(fit, col = "lightblue", conf.int = FALSE)
+  }
+  
+  fit <- survfit(Surv(real_data$os, real_data$os_event) ~ 1)
+  lines(fit, lwd = 2.5, conf.int = FALSE)
+  
+  # pfs
+  y_tilde <- yy$t_pfs_tilde[1, 1, ]
+  fit <- survfit(Surv(y_tilde, rep(1, length(y_tilde))) ~ 1)
+  plot(fit,
+       col = "lightblue",
+       xlim = c(0, 60),
+       conf.int = FALSE,
+       main = "PFS",
+       xlab = "Month",
+       bty = "n")
+  
+  for (i in 2:n_post) {
+    fit <- survfit(Surv(yy$t_pfs_tilde[1, i, ], rep(1, length(y_tilde))) ~ 1)
+    lines(fit, col = "lightblue", conf.int = FALSE)
+  }
+  
+  fit <- survfit(Surv(real_data$pfs, real_data$pfs_event) ~ 1)
+  lines(fit, lwd = 2.5, conf.int = FALSE)
+  
+  title(tx_name, line = -1, outer = TRUE)
+  dev.off()
+  
+  return()
+}
+
--- scripts/stand_alone_posterior_predictions.R
+++ scripts/stand_alone_posterior_predictions.R
@@ -57,45 +57,17 @@ res <-
 # Kaplan-Meier plots #
 ######################
 
-library(survival)
+orig_data <- load("~/R/rstanbmcm/data/surv_input_data.RData")
 
-# tx_name <- "IPILIMUMAB"
-# tx_name <- "NIVOLUMAB"
+tx_name <- "IPILIMUMAB"
+fileloc_out <- paste0("plots/post_pred_cfsep_exp_exp_", tx_name, ".png")
+plot_post_pred_KM(tx_name, orig_data, fileloc_out)
+
+tx_name <- "NIVOLUMAB"
+fileloc_out <- paste0("plots/post_pred_cfsep_exp_exp_", tx_name, ".png")
+plot_post_pred_KM(tx_name, orig_data, fileloc_out)
+
 tx_name <- "NIVOLUMAB+IPILIMUMAB"
-n_post <- 90
+fileloc_out <- paste0("plots/post_pred_cfsep_exp_exp_", tx_name, ".png")
+plot_post_pred_KM(tx_name, orig_data, fileloc_out)
 
-load("~/R/rstanbmcm/data/surv_input_data.RData")
-real_data <- surv_input_data[surv_input_data$TRTA == tx_name, ]
-
-yy <- rstan::extract(res)
-
-png(filename = paste0("plots/post_pred_cfsep_exp_exp_", tx_name, ".png"))
-par(mfrow = c(1,2))
-
-# os
-y_tilde <- yy$t_os_tilde[1, 1, ]
-fit <- survfit(Surv(y_tilde, rep(1, length(y_tilde))) ~ 1)
-plot(fit, col = "lightblue", xlim = c(0, 60), conf.int = FALSE, main = "OS", ylab = "Survival", xlab = "Month", bty = "n")
-for (i in 2:n_post) {
-  fit <- survfit(Surv(yy$t_os_tilde[1, i, ], rep(1, length(y_tilde))) ~ 1)
-  lines(fit, col = "lightblue", conf.int = FALSE)
-}
-
-fit <- survfit(Surv(real_data$os, real_data$os_event) ~ 1)
-lines(fit, lwd = 2.5, conf.int = FALSE)
-
-# pfs
-y_tilde <- yy$t_pfs_tilde[1, 1, ]
-fit <- survfit(Surv(y_tilde, rep(1, length(y_tilde))) ~ 1)
-plot(fit, col = "lightblue", xlim = c(0, 60), conf.int = FALSE, main = "PFS", xlab = "Month", bty = "n")
-
-for (i in 2:n_post) {
-  fit <- survfit(Surv(yy$t_pfs_tilde[1, i, ], rep(1, length(y_tilde))) ~ 1)
-  lines(fit, col = "lightblue", conf.int = FALSE)
-}
-
-fit <- survfit(Surv(real_data$pfs, real_data$pfs_event) ~ 1)
-lines(fit, lwd = 2.5, conf.int = FALSE)
-
-title(tx_name, line = -1, outer = TRUE)
-dev.off()

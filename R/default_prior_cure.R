a467e2bb57ae842fc60497d28ab62934a92e3ae2
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2022-10-10 20:08:15 +0100

    Update default_prior_cure.R

--- R/default_prior_cure.R
+++ R/default_prior_cure.R
@@ -1,37 +1,38 @@
 
-#' prep shared parameters
+#' Prepare shared parameters
 #'
-#' Model parameters not specific to OS or PFS:
-#' cure fraction, background and generated.
+#' Model parameters for cure fraction, background and generated.
 #'
-#' @param formula_cure
-#' @param bg_model Background model. 1: Exponential distribution; 2: fixed point values from life-tables
+#' @param formula_cure Result of parse_formula()
+#' @param bg_model Background model. 1: Exponential distribution;
+#'                                   2: fixed point values from life-tables
 #'
 #' @return list
 #'
 default_prior_cure <- function(formula_cure,
-                               bg_model = 1) {
+                               bg_model = 2) {
 
-  ##TODO:
+  ##TODO: not hard-coded
   nTx <- 3
   n_groups <- formula_cure$n_groups
   nvars <- formula_cure$nvars
 
+  # treatment fixed effect
   mu_alpha_tx <- rep(-0.6, nTx)
   sigma_alpha_tx <- rep(0.8, nTx)
 
   params_cf <-
-    if (formula_cure$cf_idx == 3) {
+    if (formula_cure$cf_idx == 3) {     # hierarchical
       list(mu_sd_cf = rep(0, nTx),
            sigma_sd_cf = rep(2.5, nTx),
            mu_alpha = mu_alpha_tx,
            sigma_alpha = sigma_alpha_tx)
-    } else if (formula_cure$cf_idx == 2) {
+    } else if (formula_cure$cf_idx == 2) {  # separate
       list(mu_alpha    = matrix(rep(mu_alpha_tx, n_groups),
                                 ncol = nTx, byrow = TRUE),
            sigma_alpha = matrix(rep(sigma_alpha_tx, n_groups),
                                 ncol = nTx, byrow = TRUE))
-    } else if (formula_cure$cf_idx == 1) {
+    } else if (formula_cure$cf_idx == 1) {  # pooled
       list(a_cf = 1,
            b_cf = 1)
     }
@@ -44,6 +45,7 @@ default_prior_cure <- function(formula_cure,
       list(mu_bg = numeric(0),
            sigma_bg = numeric(0))}
 
+  # latent submodel on coefficients
   params_beta <-
     list(mu_0 = matrix(rep(0, nvars*n_groups),
                        row = n_groups),

f7344de8431118421ce568c86ccf4bd73782e926
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2022-10-14 18:00:47 +0100

    Update misc.R

--- R/misc.R
+++ R/misc.R
@@ -4,14 +4,26 @@ cglue_data <- function(...) {
   paste0(glue_data(...), collapse = "\n")
 }
 
-# independent models
-is_separate <- formula(x)
-  x$fe_nvars == 2
-
 # single model
 is_pooled < - function(x)
   x$nvars == 1
 
+# independent models
+is_separate <- formula(x)
+  x$fe_nvars == 2
+
 # hierarchical model
 is_hier <- formula(x)
   !is.null(x$bars)
+
+# pooled cure fractions
+is_pooled_cf <- function(x)
+  x$cf_idx == 1
+
+# independent cure fractions
+is_separate_cf <- function(x)
+  x$cf_idx == 2
+
+# hierarchical cure fraction
+is_hier_cf <- function(x)
+  x$cf_idx == 3

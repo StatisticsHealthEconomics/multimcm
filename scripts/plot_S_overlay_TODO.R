0139b2233bb8f48e50de4f667c221acfd15ccea8
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2023-02-27 09:12:44 +0000

    plot distribution survival curves on same plots

--- scripts/plot_S_overlay_TODO.R
+++ scripts/plot_S_overlay_TODO.R
@@ -25,15 +25,19 @@ for (i in distns) {
   ggplot(plot_dat[[1]], aes(x = time, y = mean, group = type_tx, linetype = Tx, colour = distns[1])) +
   geom_line(size = 1.1) +
   facet_grid( ~ endpoint,
-            labeller = as_labeller(c(1 = "jj", 2 = "kkk"))) +
+            labeller = as_labeller(c("1" = "OS", "2" = "PFS"))) +
   geom_line(data = plot_dat[[2]], aes(x = time, y = mean, group = type_tx, linetype = Tx, colour = distns[2]), size = 1.1, inherit.aes = FALSE) +
   geom_line(data = plot_dat[[3]], aes(x = time, y = mean, group = type_tx, linetype = Tx, colour = distns[3]), size = 1.1, inherit.aes = FALSE) +
   geom_line(data = plot_dat[[4]], aes(x = time, y = mean, group = type_tx, linetype = Tx, colour = distns[4]), size = 1.1, inherit.aes = FALSE) +
   geom_line(data = plot_dat[[5]], aes(x = time, y = mean, group = type_tx, linetype = Tx, colour = distns[5]), size = 1.1, inherit.aes = FALSE) +
   ylab("Survival") +
+  xlab("Time (months)") +
   ylim(0, 1) +
   xlim(0,70) +
   theme_bw() +
+  labs(color = 'Distribution', linetype = "Treatment") +
     # scale_colour_calc()
   scale_colour_hc()
 
+ggsave(filename = here::here("plots", "plot_S_overlay.png"),
+         units = "in", width = 12, height = 6, dpi = 300)

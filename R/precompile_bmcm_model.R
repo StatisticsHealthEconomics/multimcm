edde06ece709250c196e5c467737ace50ab51141
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2024-08-02 17:48:00 +0100

    created precompile_bmcm_model() so dont have to recompile in simulation study.
    * still need to change bmcm_stan() to take precompile object

---
+++ R/precompile_bmcm_model.R
@@ -0,0 +1,27 @@
+
+#
+precompile_bmcm_model <- function(input_data,
+                                  family_latent = "exponential",
+                                  cureformula = ~ 1) {
+
+  distns <- validate_distns(family_latent)
+
+  formula_cure <- parse_formula(cureformula, input_data)
+
+  if (length(distns) == 1) {
+    if (is_hier(formula_cure)) {
+      distns <- rep(distns, formula_cure$re_nlevels[1])
+    } else if (is_separate(formula_cure)) {
+      distns <- rep(distns, formula_cure$fe_nlevels[2])
+    }}
+
+  model_code <- create_stancode(distns)
+
+  model_name <-
+    paste0("bmcm_stan_", glue::glue_collapse(distns, sep = "_"))
+
+  precompiled_model <- stan_model(model_code = model_code,
+                                  model_name = model_name)
+
+  saveRDS(precompiled_model, file = glue::glue("{model_name}.RDS"))
+}
